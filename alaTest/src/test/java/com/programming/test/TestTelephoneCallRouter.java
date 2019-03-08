abf916012c08f6fc60ee81ee8cac6dc72c9513b2
prashanna <prashanna@fusemachines.com>
2019-03-08 12:44:38 +0100

    added more positive and negative test case

--- alaTest/src/test/java/com/programming/test/TestTelephoneCallRouter.java
+++ alaTest/src/test/java/com/programming/test/TestTelephoneCallRouter.java
@@ -54,6 +54,77 @@ public class TestMyTelephoneCallRouter extends TestCase {
 	    
 	}
 
+
+	@Test
+	public void test_expect_cheap_price_from_operatorA_when_phonePrefix_is_1() {
+		Long phonenumber = new Long("1603312345");	
+		
+		List<OperatorEntry> seedOperatorentrylist = new ArrayList<OperatorEntry>();
+		seedOperatorentrylist.add(new OperatorEntry("OperatorA",new Integer(1),new Double(0.9)));
+		seedOperatorentrylist.add(new OperatorEntry("OperatorB",new Integer(1),new Double(0.92)));
+
+		Map<String,String> expectedcheapOperator = new HashMap();
+		expectedcheapOperator.put("Operator","OperatorA");
+		expectedcheapOperator.put("price","0.9");
+	    when(mockCSVReader.initalizeOperatorEntryfromCSVFiles()).thenReturn(seedOperatorentrylist);
+		this.myTelephoneCallRouter = new MyTelephoneCallRouter(mockCSVReader);
+	    Map<String,String> actualcheapPrice=myTelephoneCallRouter.getCheapOperator(phonenumber);
+	    assertThat(actualcheapPrice, is(expectedcheapOperator));
+	}
+
+	@Test
+	public void test_expect_cheap_price_from_operatorA_when_phonePrefix_is_46() {
+		Long phonenumber = new Long("4603312345");	
+
+		List<OperatorEntry> seedOperatorentrylist = new ArrayList<OperatorEntry>();
+		seedOperatorentrylist.add(new OperatorEntry("OperatorA",new Integer(46),new Double(0.17)));
+		seedOperatorentrylist.add(new OperatorEntry("OperatorB",new Integer(46),new Double(0.2)));
+
+		Map<String,String> expectedcheapOperator = new HashMap();
+		expectedcheapOperator.put("Operator","OperatorA");
+		expectedcheapOperator.put("price","0.17");
+	    when(mockCSVReader.initalizeOperatorEntryfromCSVFiles()).thenReturn(seedOperatorentrylist);
+		this.myTelephoneCallRouter = new MyTelephoneCallRouter(mockCSVReader);
+
+	    Map<String,String> actualcheapPrice=myTelephoneCallRouter.getCheapOperator(phonenumber);
+	    assertThat(actualcheapPrice, is(expectedcheapOperator));
+	}
+
+	@Test
+	public void test_expect_cheap_price_from_operatorB_when_phonePrefix_is_44() {
+		Long phonenumber = new Long("4483312345");
+		
+		List<OperatorEntry> seedOperatorentrylist = new ArrayList<OperatorEntry>();
+		seedOperatorentrylist.add(new OperatorEntry("OperatorB",new Integer(44),new Double(0.5)));
+		
+		Map<String,String> expectedcheapOperator = new HashMap();
+		expectedcheapOperator.put("Operator","OperatorB");
+		expectedcheapOperator.put("price","0.5");
+	    
+		when(mockCSVReader.initalizeOperatorEntryfromCSVFiles()).thenReturn(seedOperatorentrylist);
+		this.myTelephoneCallRouter = new MyTelephoneCallRouter(mockCSVReader);
+		
+	    Map<String,String> actualcheapPrice=myTelephoneCallRouter.getCheapOperator(phonenumber);
+	    assertThat(actualcheapPrice, is(expectedcheapOperator));
+	}
+
+	
+	@Test
+	public void test_expect_no_price_from_both_operator_when_phonePrefix_is_not_in_list_in_operators() {
+		Long phonenumber = new Long("0083312345");	
+		
+		List<OperatorEntry> seedOperatorentrylist = new ArrayList<OperatorEntry>();
+		seedOperatorentrylist.add(new OperatorEntry("OperatorA",new Integer(46),new Double(0.17)));
+		seedOperatorentrylist.add(new OperatorEntry("OperatorB",new Integer(44),new Double(0.5)));
+		
+		Map<String,String> expectedcheapOperator = new HashMap();
+		when(mockCSVReader.initalizeOperatorEntryfromCSVFiles()).thenReturn(seedOperatorentrylist);
+		this.myTelephoneCallRouter = new MyTelephoneCallRouter(mockCSVReader);
+
+	    Map<String,String> actualcheapPrice=myTelephoneCallRouter.getCheapOperator(phonenumber);
+	    assertThat(actualcheapPrice, is(expectedcheapOperator));
+	}
+
 }
 
 
